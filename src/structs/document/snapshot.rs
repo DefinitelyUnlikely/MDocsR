c443c413c8047688ee1daa5e3311207f6ea28d92
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-05-26 20:49:04 +0200

    add snapshots

--- src/structs/document/snapshot.rs
+++ src/structs/document/snapshot.rs
@@ -1,5 +1,9 @@
 use chrono::{DateTime, Utc};
 
 pub struct Snapshot {
-    
+    pub id: String,
+    pub document_id: String,
+    pub user_id: String,
+    pub data: Vec<u8>,
+    pub created_at: DateTime<Utc>,
 }
