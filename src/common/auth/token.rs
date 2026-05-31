3f87253d874c544c51543a86d368477403926ad1
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-05-31 21:54:05 +0200

    change to a str ref instead of string, I should probably not take ownership of the userid?

--- src/common/auth/token.rs
+++ src/common/auth/token.rs
@@ -10,14 +10,14 @@ struct Claims {
     sub: String,
 }
 
-fn create_jwt(user_id: String) -> Result<String, jsonwebtoken::errors::Error> {
+fn create_jwt(user_id: &str) -> Result<String, jsonwebtoken::errors::Error> {
     let now = Utc::now().timestamp() as usize;
 
     let claims = Claims {
         aud: String::from("TODO: Make use of config"),
         exp: now + 900, // TODO: Can also be a config for the duration we add
         iss: String::from("TODO: Make use of config for iss"),
-        sub: user_id.clone(),
+        sub: user_id.to_string(),
     };
 
     let secret = EncodingKey::from_secret("super_secret_key".as_ref());
