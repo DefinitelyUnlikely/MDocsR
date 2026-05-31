69c46254cea2fba7e3233ac1d4ff4ccfd55bc10c
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-05-31 18:34:15 +0200

    add the bcrypt calls and return the correct result type for verify

--- src/common/auth/crypto.rs
+++ src/common/auth/crypto.rs
@@ -1,7 +1,9 @@
 use bcrypt::{BcryptError, DEFAULT_COST};
 
 pub fn hash_password(password: &str) -> Result<String, BcryptError> {
+    bcrypt::hash(password, DEFAULT_COST)
 }
 
-pub fn verify_password(password: &str, hash: &str) -> Result<String, BcryptError> {
+pub fn verify_password(password: &str, hash: &str) -> Result<bool, BcryptError> {
+    bcrypt::verify(password, hash)
 }
