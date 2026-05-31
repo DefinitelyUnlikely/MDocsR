f4515e8a02dac7bac13623308b2d8b78dfe3ca0d
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-05-31 20:12:21 +0200

    add rand rng to generate random bytes, add hex to encode them and then add in duration to add a 7 day expiration day

--- src/common/auth/refresh_token.rs
+++ src/common/auth/refresh_token.rs
@@ -1,7 +1,25 @@
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Duration, Utc};
+use rand::prelude::*;
 
 pub struct RefreshToken {
     pub token: String,
     pub user_id: String,
     pub expires: DateTime<Utc>,
 }
+
+impl RefreshToken {
+    pub fn new(user_id: String) -> Self {
+        RefreshToken {
+            token: Self::generate_token_value(),
+            user_id,
+            expires: Utc::now() + Duration::days(7),
+        }
+    }
+
+    pub fn generate_token_value() -> String {
+        let mut bytes = [0u8; 32];
+        rand::rng().fill_bytes(&mut bytes);
+        hex::encode(bytes)
+    }
+}
+
