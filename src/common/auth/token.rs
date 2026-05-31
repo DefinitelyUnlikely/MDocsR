be65c1fc776ec2134a00f9edb3813f1e273c64cf
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-05-31 21:51:10 +0200

    add basic jwt generation using symmetric signning (for now) based on the readme. TODO: change values to config values

--- src/common/auth/token.rs
+++ src/common/auth/token.rs
@@ -1,5 +1,6 @@
 use serde::{Serialize, Deserialize};
 use jsonwebtoken::{encode, decode, Header, Algorithm, Validation, EncodingKey, DecodingKey};
+use chrono::{Duration, Utc};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Claims {
@@ -7,4 +8,18 @@ struct Claims {
     exp: usize,
     iss: String,
     sub: String,
+}
+
+fn create_jwt(user_id: String) -> Result<String, jsonwebtoken::errors::Error> {
+    let now = Utc::now().timestamp() as usize;
+
+    let claims = Claims {
+        aud: String::from("TODO: Make use of config"),
+        exp: now + 900, // TODO: Can also be a config for the duration we add
+        iss: String::from("TODO: Make use of config for iss"),
+        sub: user_id.clone(),
+    };
+
+    let secret = EncodingKey::from_secret("super_secret_key".as_ref());
+    encode(&Header::default(), &claims, &secret)
 }
