bbed2639ff1b262c988c68b4754c103dfc9bee7c
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-06-08 21:22:04 +0200

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use crate::db::create_pool;
 use std::env;
 
 pub mod common;
-pub mod features;
 mod db;
+pub mod features;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    create_pool(&database_url).await.expect("Failed to create database pool");
+    create_pool(&database_url)
+        .await
+        .expect("Failed to create database pool");
 }
