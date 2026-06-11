aea3973a45d2234eec989866f2c6ff6884188bb7
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:44:55 +0200

    make the branching simple to start with, make more rust idiomatic later

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -3,12 +3,18 @@ use crate::common::error::Error;
 use crate::features::users::user::User;
 
 pub async fn fetch_user_by_id(id: String, pool: &PgPool, include_info: bool) -> Result<User, Error> {
-    if include_info {}
-    
-    let fetch = fetch_user_without_info(id, pool).await;
-    match fetch {
-        Ok(user) => Ok(user),
-        Err(err) => Err(err.into()),
+    if include_info {
+        let fetch = fetch_user_with_info(id, pool).await;
+        match fetch {
+            Ok(user) => Ok(user),
+            Err(err) => Err(err.into()),
+        }
+    } else {
+        let fetch = fetch_user_without_info(id, pool).await;
+        match fetch {
+            Ok(user) => Ok(user),
+            Err(err) => Err(err.into()),
+        }
     }
 }
 
