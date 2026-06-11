df2470dca71eb3bd9d7b96a9ef7d7fafa4f943d9
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:48:19 +0200

    update the with info error type return

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -12,8 +12,11 @@ pub async fn fetch_user_by_id(id: String, pool: &PgPool, include_info: bool) -> 
     Ok(user)
 }
 
-async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, Error> {
-    todo!()
+async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, sqlx::Error> {
+    let result = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
+        .fetch_one(pool)
+        .await;
+    result
 }
 
 async fn fetch_user_without_info(id: String, pool: &PgPool) -> Result<User, sqlx::Error> {
