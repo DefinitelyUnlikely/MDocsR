e3ba9498a14a7674898e71f12ec8bb5b79a01850
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:54:23 +0200

    remove the with info stuff, as we return a user in the result. We'll make a separate function that returns UserWithInfo making them completetly separate

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -2,26 +2,10 @@ use sqlx::{query_as, PgPool};
 use crate::common::error::Error;
 use crate::features::users::user::User;
 
-pub async fn fetch_user_by_id(id: String, pool: &PgPool, include_info: bool) -> Result<User, Error> {
-    let user = if include_info {
-        fetch_user_with_info(id, pool).await?
-    } else {
-        fetch_user_without_info(id, pool).await?
-    };
-    
+pub async fn fetch_user_by_id(id: String, pool: &PgPool) -> Result<User, Error> {
+    let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
+        .fetch_one(pool)
+        .await?;
+
     Ok(user)
-}
-
-async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, sqlx::Error> {
-    let result = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
-        .fetch_one(pool)
-        .await;
-    result
-}
-
-async fn fetch_user_without_info(id: String, pool: &PgPool) -> Result<User, sqlx::Error> {
-    let result = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
-        .fetch_one(pool)
-        .await;
-    result
 }
