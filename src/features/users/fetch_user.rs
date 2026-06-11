d594e93b125e72a40677ef1f1689d096939d93ec
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:13:52 +0200

    add the chrono crate to sqlx

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -6,8 +6,10 @@ pub async fn fetch_user_by_id(id: String, pool: &PgPool, include_info: bool) -> 
     todo!()
 }
 
-async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, Error> {}
+async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, Error> {
+    todo!()
+}
 
 async fn fetch_user_without_info(id: String, pool: &PgPool) -> Result<User, Error> {
-    
+    let result = sqlx::query_as!(crate::features::users::user::User, "SELECT * FROM users WHERE id = $1", id);
 }
