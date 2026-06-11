be1a7eb5968281ffb6f26d74c2362a29fbfea800
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:00:39 +0200

    add todo to fetch user as well

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -3,7 +3,5 @@ use crate::common::error::Error;
 use crate::features::users::user::User;
 
 pub async fn fetch_user_by_id(id: String, pool: &PgPool) -> Result<User, Error> {
-    let result = query_as!(User, "SELECT * FROM users WHERE id = $1", id)
-        .fetch_one(&pool)
-        .await;
+    todo!()
 }
