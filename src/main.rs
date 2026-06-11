4740a8455bd39bc7daa0c3cd8ae5fa9ee9451abe
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 14:09:17 +0200

    make the pool an actual variable

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
     dotenvy::dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    create_pool(&database_url)
+    let pool = create_pool(&database_url)
         .await
         .expect("Failed to create database pool");
 }
