8d071d22942bcb021cccc087c4e4bb9b41e043d2
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:47:17 +0200

    let ai transalte it into a more idiomatic rust code block

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -3,19 +3,13 @@ use crate::common::error::Error;
 use crate::features::users::user::User;
 
 pub async fn fetch_user_by_id(id: String, pool: &PgPool, include_info: bool) -> Result<User, Error> {
-    if include_info {
-        let fetch = fetch_user_with_info(id, pool).await;
-        match fetch {
-            Ok(user) => Ok(user),
-            Err(err) => Err(err.into()),
-        }
+    let user = if include_info {
+        fetch_user_with_info(id, pool).await?
     } else {
-        let fetch = fetch_user_without_info(id, pool).await;
-        match fetch {
-            Ok(user) => Ok(user),
-            Err(err) => Err(err.into()),
-        }
-    }
+        fetch_user_without_info(id, pool).await?
+    };
+    
+    Ok(user)
 }
 
 async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, Error> {
