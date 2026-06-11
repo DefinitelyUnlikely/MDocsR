56b01c7cddd6264d8f911175b8f7c5e19467271f
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-11 15:07:14 +0200

    split user fetching into two sub functions

--- src/features/users/fetch_user.rs
+++ src/features/users/fetch_user.rs
@@ -2,6 +2,12 @@ use sqlx::{query_as, PgPool};
 use crate::common::error::Error;
 use crate::features::users::user::User;
 
-pub async fn fetch_user_by_id(id: String, pool: &PgPool) -> Result<User, Error> {
+pub async fn fetch_user_by_id(id: String, pool: &PgPool, include_info: bool) -> Result<User, Error> {
     todo!()
+}
+
+async fn fetch_user_with_info(id: String, pool: &PgPool) -> Result<User, Error> {}
+
+async fn fetch_user_without_info(id: String, pool: &PgPool) -> Result<User, Error> {
+    
 }
