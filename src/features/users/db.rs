9829aa1ba2aa94c83eb2535d2d3dc53e5a11a0ef
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-23 15:43:15 +0200

    switch to making use of fetch_optional as that combines the idea of option from refresh token and result from user into one

--- src/features/users/db.rs
+++ src/features/users/db.rs
@@ -2,10 +2,10 @@ use crate::common::error::Error;
 use crate::features::users::user::User;
 use sqlx::PgPool;
 
-pub async fn fetch_user_by_id(id: &str, pool: &PgPool) -> Result<User, Error> {
+pub async fn fetch_user_by_id(id: &str, pool: &PgPool) -> Result<Option<User>, Error> {
     println!("Fetching user by id {}", id);
     let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
-        .fetch_one(pool)
+        .fetch_optional(pool)
         .await?;
 
     Ok(user)
