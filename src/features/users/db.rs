5804c35da28257ba8362053c5b37355851482445
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-23 15:47:00 +0200

    and move fetch usre by id into a trait impl

--- src/features/users/db.rs
+++ src/features/users/db.rs
@@ -2,11 +2,22 @@ use crate::common::error::Error;
 use crate::features::users::user::User;
 use sqlx::PgPool;
 
-pub async fn fetch_user_by_id(id: &str, pool: &PgPool) -> Result<Option<User>, Error> {
-    println!("Fetching user by id {}", id);
-    let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
-        .fetch_optional(pool)
-        .await?;
-
-    Ok(user)
+struct UserRepository {
+    pool: PgPool,
 }
+
+impl UserRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+
+    pub async fn fetch_user_by_id(&self, id: &str) -> Result<Option<User>, Error> {
+        println!("Fetching user by id {}", id);
+        let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
+            .fetch_optional(&self.pool)
+            .await?;
+
+        Ok(user)
+    }
+}
+
