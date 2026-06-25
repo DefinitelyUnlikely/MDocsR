06277a6c3b2e659370a919b0add7ee43495e4ef5
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-06-25 15:27:12 +0200

    refactor parts of refresh_tokens handler, work on the branch were cookie existed

--- src/common/auth/routes.rs
+++ src/common/auth/routes.rs
@@ -8,6 +8,9 @@ use axum::{
 };
 use axum_extra::extract::CookieJar;
 use axum_extra::extract::cookie::Cookie;
+use crate::common::auth::tokens::refresh_token::db::RefreshTokenRepository;
+use crate::common::auth::tokens::tokens_service::TokensService;
+use crate::features::users::db::UserRepository;
 
 pub fn router() -> Router<AppState> {
     Router::new().route("/refresh-tokens", post(refresh_tokens))
@@ -17,8 +20,13 @@ async fn refresh_tokens(State(state): State<AppState>, jar: CookieJar) -> impl I
     let Some(token) = jar.get("refresh_token") else {
         return (StatusCode::BAD_REQUEST, "No refresh token in cookies").into_response();
     };
+    let token_repo = RefreshTokenRepository::new(state.db_pool.clone());
+    let user_repo = UserRepository::new(state.db_pool.clone());
 
-    // TODO: Implement this branch
+    let token_service = TokensService::new(token_repo, user_repo);
+    let tokens = token_service.refresh_tokens(&token.value()).await;
+    
+
     let new_jar = jar
         .add(Cookie::new("access_token", "placeholder"))
         .add(Cookie::new("refresh_token", "placeholder"));
