78884624e003b814bcb566e9fa6e37a03c27da40
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-06-30 14:44:37 +0200

    use migrate instead of sql

--- src/db.rs
+++ src/db.rs
@@ -10,47 +10,8 @@ pub async fn create_pool(database_url: &str) -> Result<PgPool, sqlx::Error> {
         .await
 }
 
-pub async fn migrate(pool: &PgPool) -> Result<(), sqlx::Error> {
-    let mut transaction= pool.begin().await?;
-
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS users 
-        (id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) UNIQUE NOT NULL,
-        email_verified BOOLEAN UNIQUE NOT NULL DEFAULT FALSE,
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP);"
-    )
-    .execute(&mut *transaction)
-    .await?;
-
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS user_infos (
-    id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-    user_id VARCHAR(255) UNIQUE NOT NULL REFERENCES users ON DELETE CASCADE,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    phone VARCHAR(50),
-    country VARCHAR (100) NOT NULL,
-    city VARCHAR (100) NOT NULL,
-    state VARCHAR (100),
-    address VARCHAR (255) NOT NULL,
-    postal_code VARCHAR(20) NOT NULL
-    );"
-    )
-    .execute(&mut *transaction)
-    .await?;
-
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS refresh_tokens (
-    token VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-    user_id VARCHAR(255) NOT NULL REFERENCES users ON DELETE CASCADE,
-    expires TIMESTAMP WITH TIME ZONE NOT NULL);"
-    )
-    .execute(&mut *transaction)
-    .await?;
-    
-    transaction.commit().await?;
-    Ok(())
+pub async fn migrate(pool: &PgPool) -> Result<(), sqlx::migrate::MigrateError> {
+    sqlx::migrate!("./migrations")
+        .run(pool)
+        .await
 }
