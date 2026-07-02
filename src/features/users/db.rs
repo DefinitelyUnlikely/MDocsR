17272ffe58c3367417a577c38dfb78712b4bbac8
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-02 19:39:36 +0200

    add way to get user by email

--- src/features/users/db.rs
+++ src/features/users/db.rs
@@ -5,6 +5,7 @@ use sqlx::PgPool;
 #[allow(async_fn_in_trait)]
 pub trait UserRepository: Send + Sync {
     async fn fetch_user_by_id(&self, id: &str) -> Result<Option<User>, Error>;
+    async fn fetch_user_by_email(&self, email: &str) -> Result<Option<User>, Error>;
 }
 
 pub struct PostgresUserRepository {
@@ -19,11 +20,18 @@ impl PostgresUserRepository {
 
 impl UserRepository for PostgresUserRepository {
     async fn fetch_user_by_id(&self, id: &str) -> Result<Option<User>, Error> {
-        println!("Fetching user by id {}", id);
         let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
             .fetch_optional(&self.pool)
             .await?;
 
+        Ok(user)
+    }
+    
+    async fn fetch_user_by_email(&self, email: &str) -> Result<Option<User>, Error> {
+        let user = sqlx::query_as!(User, "SELECT * FROM users WHERE email = $1", email)
+        .fetch_optional(&self.pool)
+        .await?;
+        
         Ok(user)
     }
 }
