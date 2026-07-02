a19221cc44a41f628a179712728f05d893293778
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-02 21:40:49 +0200

    add state to register handler and create the struct for a passkey service

--- src/common/auth/passkeys/handlers.rs
+++ src/common/auth/passkeys/handlers.rs
@@ -1,9 +1,12 @@
+use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::Json;
+use crate::AppState;
 use crate::common::dtos::passkeys::RegisterPasskeyRequest;
+use crate::features::users::db::PostgresUserRepository;
 
-pub async fn register_start(Json(payload): Json<RegisterPasskeyRequest>) -> impl IntoResponse {
-    "Pong".to_string()
+pub async fn register_start(Json(payload): Json<RegisterPasskeyRequest>, State(state): State<AppState>) -> impl IntoResponse {
+    let user_repo = PostgresUserRepository::new(state.db_pool.clone());
 }
 
 pub async fn register_finish() -> impl IntoResponse {
