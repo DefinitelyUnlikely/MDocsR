9a514d4afc56bb224376593ca144fe07dd52240d
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-02 22:24:38 +0200

    check if user exists, mark session as dummy if so

--- src/common/auth/passkeys/handlers.rs
+++ src/common/auth/passkeys/handlers.rs
@@ -4,25 +4,35 @@ use axum::response::IntoResponse;
 use axum::Json;
 use crate::AppState;
 use crate::common::dtos::passkeys::RegisterPasskeyRequest;
-use crate::features::users::db::PostgresUserRepository;
+use crate::features::users::db::{PostgresUserRepository, UserRepository};
 use regex::Regex;
+use uuid::Uuid;
+use webauthn_rs::fake::WebauthnFakeCredentialGenerator;
 use crate::common::error::Error;
 
 pub async fn register_start(Json(payload): Json<RegisterPasskeyRequest>, State(state): State<AppState>) -> Result<impl IntoResponse, Error> {
     // start by doing everything in the handler
     // and we can separate the concerns afterward.
-    
+
     // Let's start with using a simple regex to check if the email
     // is at least email-ish.
     let re = Regex::new(r"^[^@\s]+@[^@\s]+\.[^@\s]+$").unwrap();
-    
     let is_match = re.is_match(&payload.email);
     if !is_match {
         return Ok(StatusCode::BAD_REQUEST.into_response());
     }
-    
+
     let user_repo = PostgresUserRepository::new(state.db_pool.clone());
     
+    // If we find a user, they cannot register using this endpoint.
+    // but to prevent user enumeration, we store that this session is
+    // a dummy registration. We still return a real challenge. 
+    let (user_id, is_dummy) = match user_repo.fetch_user_by_email(&payload.email).await? {
+        Some(user) => (user.id, true),
+        None => (Uuid::new_v4().to_string(), false),
+    };
+
+
 }
 
 pub async fn register_finish() -> impl IntoResponse {
