bf15d7acfcd2df4d5774a8b33bbedcf4092c95f8
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-03 10:48:13 +0200

    add delete function

--- src/common/nonce.rs
+++ src/common/nonce.rs
@@ -1,6 +1,6 @@
 use std::result;
 use chrono::{DateTime, Utc};
-use sqlx::PgPool;
+use sqlx::{Error, PgPool};
 use textnonce::TextNonce;
 
 #[derive(sqlx::FromRow, Clone, Debug)]
@@ -25,8 +25,9 @@ impl RegistrationNonce {
 //-----------------
 #[allow(async_fn_in_trait)]
 pub trait RegistrationNonceRepository: Send + Sync {
-    async fn find_nonce(&self, nonce: &str) -> Result<Option<RegistrationNonce>, sqlx::Error>;
+    async fn find_registration_nonce(&self, nonce: &str) -> Result<Option<RegistrationNonce>, sqlx::Error>;
     async fn save_registration_nonce(&self, nonce: &RegistrationNonce) -> Result<bool, sqlx::Error>;
+    async fn delete_registration_nonce(&self, nonce: &str) -> Result<(), sqlx::Error>;
 }
 
 pub struct PostgresRegistrationNonceRepository {
@@ -40,10 +41,10 @@ impl PostgresRegistrationNonceRepository {
 }
 
 impl RegistrationNonceRepository for PostgresRegistrationNonceRepository {
-    async fn find_nonce(&self, email: &str) -> Result<Option<RegistrationNonce>, sqlx::Error> {
+    async fn find_registration_nonce(&self, email: &str) -> Result<Option<RegistrationNonce>, sqlx::Error> {
         let result = sqlx::query_as!(RegistrationNonce, "SELECT * FROM registration_nonces WHERE nonce = $1", email);
     }
-    
+
     async fn save_registration_nonce(&self, nonce: &RegistrationNonce) -> Result<bool, sqlx::Error> {
         let result = sqlx::query!(
             "INSERT INTO registration_nonces VALUES ($1, $2, $3)",
@@ -56,6 +57,12 @@ impl RegistrationNonceRepository for PostgresRegistrationNonceRepository {
 
         Ok(result.rows_affected() == 1)
     }
-    
 
+    async fn delete_registration_nonce(&self, nonce: &str) -> Result<(), Error> {
+        sqlx::query!("DELETE FROM registration_nonces WHERE nonce = $1", nonce)
+            .execute(&self.pool)
+            .await?;
+        
+        Ok(())
+    }
 }
