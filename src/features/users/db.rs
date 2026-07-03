2ce60df12506648108def9ebe9409635f7102816
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-03 15:10:54 +0200

    add a save user to repo

--- src/features/users/db.rs
+++ src/features/users/db.rs
@@ -4,6 +4,7 @@ use sqlx::PgPool;
 
 #[allow(async_fn_in_trait)]
 pub trait UserRepository: Send + Sync {
+    async fn save_user(&self, user: User) -> Result<(), Error>;
     async fn fetch_user_by_id(&self, id: &str) -> Result<Option<User>, Error>;
     async fn fetch_user_by_email(&self, email: &str) -> Result<Option<User>, Error>;
 }
@@ -19,6 +20,18 @@ impl PostgresUserRepository {
 }
 
 impl UserRepository for PostgresUserRepository {
+    async fn save_user(&self, user: User) -> Result<(), Error> {
+        sqlx::query!(
+            "INSERT INTO users (id, email) VALUES ($1, $2)",
+            user.id,
+            user.email
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+
     async fn fetch_user_by_id(&self, id: &str) -> Result<Option<User>, Error> {
         let user = sqlx::query_as!(User, "SELECT * FROM users WHERE id = $1", id)
             .fetch_optional(&self.pool)
--- src/features/users/service.rs
+++ src/features/users/service.rs
@@ -4,7 +4,10 @@ pub struct UserService<U> {
     user_repo: U,
 }
 
-impl<U> UserService<U> where U: UserRepository {
+impl<U> UserService<U>
+where
+    U: UserRepository,
+{
     pub fn new(user_repo: U) -> Self {
         UserService { user_repo }
     }
