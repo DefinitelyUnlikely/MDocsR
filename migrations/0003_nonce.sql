88eabd9b86303a6ae82485e6dad18b24cda77e7e
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-03 10:37:52 +0200

    update sql

--- migrations/0003_nonce.sql
+++ migrations/0003_nonce.sql
@@ -1,1 +1,6 @@
-CREATE TABLE IF NOT EXISTS registration_nonces (nonce VARCHAR(255), email VARCHAR(255));
+CREATE TABLE IF NOT EXISTS registration_nonces (
+    nonce VARCHAR(255) PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL
+                                               );
+
--- src/common/nonce.rs
+++ src/common/nonce.rs
@@ -1,4 +1,5 @@
 use chrono::{DateTime, Utc};
+use sqlx::PgPool;
 use textnonce::TextNonce;
 
 #[derive(sqlx::FromRow, Clone, Debug)]
@@ -15,5 +16,29 @@ impl RegistrationNonce {
             email,
             created_at: Utc::now(),
         }
+    }
+}
+
+//-----------------
+// NONCE DATABASE
+//-----------------
+#[allow(async_fn_in_trait)]
+pub trait RegistrationNonceRepository: Send + Sync {
+    async fn find_nonce(&self, nonce: &str) -> Result<Option<RegistrationNonce>, sqlx::Error>;
+}
+
+pub struct PostgresRegistrationNonceRepository {
+    pool: PgPool,
+}
+
+impl PostgresRegistrationNonceRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+impl RegistrationNonceRepository for PostgresRegistrationNonceRepository {
+    async fn find_nonce(&self, email: &str) -> Result<Option<RegistrationNonce>, sqlx::Error> {
+        let result: Option<RegistrationNonce> = sqlx::query_as!(RegistrationNonce, "SELECT * FROM registration_nonces WHERE nonce = $1", email);
     }
 }
