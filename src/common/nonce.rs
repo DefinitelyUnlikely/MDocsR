40dc0b045f8800f9a869bf472b886d30f943b1d8
DefinitelyUnlikely <martin@kaarjohansson.se>
2026-07-03 10:45:10 +0200

    add a way to save nonces

--- src/common/nonce.rs
+++ src/common/nonce.rs
@@ -1,3 +1,4 @@
+use std::result;
 use chrono::{DateTime, Utc};
 use sqlx::PgPool;
 use textnonce::TextNonce;
@@ -25,6 +26,7 @@ impl RegistrationNonce {
 #[allow(async_fn_in_trait)]
 pub trait RegistrationNonceRepository: Send + Sync {
     async fn find_nonce(&self, nonce: &str) -> Result<Option<RegistrationNonce>, sqlx::Error>;
+    async fn save_registration_nonce(&self, nonce: &RegistrationNonce) -> Result<bool, sqlx::Error>;
 }
 
 pub struct PostgresRegistrationNonceRepository {
@@ -39,6 +41,21 @@ impl PostgresRegistrationNonceRepository {
 
 impl RegistrationNonceRepository for PostgresRegistrationNonceRepository {
     async fn find_nonce(&self, email: &str) -> Result<Option<RegistrationNonce>, sqlx::Error> {
-        let result: Option<RegistrationNonce> = sqlx::query_as!(RegistrationNonce, "SELECT * FROM registration_nonces WHERE nonce = $1", email);
+        let result = sqlx::query_as!(RegistrationNonce, "SELECT * FROM registration_nonces WHERE nonce = $1", email);
     }
+    
+    async fn save_registration_nonce(&self, nonce: &RegistrationNonce) -> Result<bool, sqlx::Error> {
+        let result = sqlx::query!(
+            "INSERT INTO registration_nonces VALUES ($1, $2, $3)",
+            nonce.nonce,
+            nonce.email,
+            nonce.created_at
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(result.rows_affected() == 1)
+    }
+    
+
 }
