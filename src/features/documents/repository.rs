3f910a307f1dace3936b3ecf237e0ff2b6c00369
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-07-10 13:22:17 +0200

    update trait with async, implement the C in CRUD

--- src/features/documents/repository.rs
+++ src/features/documents/repository.rs
@@ -3,12 +3,40 @@ use sqlx::PgPool;
 use crate::features::documents::document::Document;
 
 pub trait DocumentRepository: Send + Sync {
-    fn save(&self, document: Document) -> Result<(), Error>;
+    async fn save(&self, document: Document) -> Result<(), Error>;
+
+    async fn get_by_id(&self, document_id: &str) -> Result<Document, Error>;
+    async fn get_all_user_documents(&self, user_id: &str) -> Result<Vec<Document>, Error>;
+    async fn get_all(&self) -> Result<Vec<Document>, Error>;
+
+    async fn update(&self, document: Document) -> Result<bool, Error>;
+    async fn delete(&self, document_id: &str) -> Result<(), Error>;
+}
+
+pub struct PostgresDocumentRepository {
+    pool: PgPool,
+}
+
+impl PostgresDocumentRepository {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+impl DocumentRepository for PostgresDocumentRepository {
     
-    fn get_by_id(&self, document_id: &str) -> Result<Document, Error>;
-    fn get_all_user_documents(&self, user_id: &str) -> Result<Vec<Document>, Error>;
-    fn get_all(&self) -> Result<Vec<Document>, Error>;
-    
-    fn update(&self, document: Document) -> Result<bool, Error>;
-    fn delete(&self, document_id: &str) -> Result<(), Error>;
+    async fn save(&self, document: Document) -> Result<(), Error> {
+        sqlx::query!(
+            "INSERT INTO documents (id, user_id, title, is_tombstone, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)",
+        document.id,
+        document.user_id,
+        document.title,
+        document.is_tombstone,
+        document.created_at,
+        document.updated_at)
+            .execute(&self.pool)
+            .await?;
+        
+        Ok(())
+    }
 }
