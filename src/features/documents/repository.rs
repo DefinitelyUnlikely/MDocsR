f79810b7f7c0bf622a9bda725ec8dbe6384831ac
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-07-10 13:32:22 +0200

    add the D of crud

--- src/features/documents/repository.rs
+++ src/features/documents/repository.rs
@@ -89,5 +89,11 @@ impl DocumentRepository for PostgresDocumentRepository {
         }
     }
 
-    async fn delete(&self, document_id: &str) -> Result<(), Error> {}
+    async fn delete(&self, document_id: &str) -> Result<(), Error> {
+        sqlx::query!("DELETE FROM documents WHERE id = $1", document_id)
+            .execute(&self.pool)
+        .await?;
+        
+        Ok(())
+    }
 }
