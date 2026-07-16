6bb3a2c9eb22b32f333145725e623f914ccfcc98
Martin Kåår Johansson <martin@kaarjohansson.se>
2026-07-16 14:23:27 +0200

    add a route for new doc

--- src/features/documents/routes.rs
+++ src/features/documents/routes.rs
@@ -1,13 +1,14 @@
 use axum::extract::State;
 use axum::response::IntoResponse;
 use axum::Router;
+use axum::routing::post;
 use crate::AppState;
 use crate::common::auth::extractor::AuthenticatedUser;
 use crate::common::error::Error;
 
 pub fn documents_router() -> Router<AppState> {
     Router::new()
-        .route("/new")
+        .route("/new", post(new_document))
 }
 
 async fn new_document(user: AuthenticatedUser, State(state): State<AppState>) -> Result<impl IntoResponse, Error> {
